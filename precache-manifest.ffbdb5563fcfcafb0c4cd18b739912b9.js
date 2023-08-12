0093af99a5dd24cb475e132418d724a6dba49242
Roushan7970835758 <rroushan40@gmail.com>
2023-08-12 14:30:15 +0530

    Updates

---
+++ precache-manifest.ffbdb5563fcfcafb0c4cd18b739912b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6818cdf20706bb849f14",
+    "url": "/NewsMonkey/static/css/main.fafa8852.chunk.css"
+  },
+  {
+    "revision": "6818cdf20706bb849f14",
+    "url": "/NewsMonkey/static/js/main.6d7a8505.chunk.js"
+  },
+  {
+    "revision": "75e28b5b988fe68a865d",
+    "url": "/NewsMonkey/static/js/runtime~main.0f92303a.js"
+  },
+  {
+    "revision": "8f0fbf767cbfee435666",
+    "url": "/NewsMonkey/static/js/2.9ee70e49.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/NewsMonkey/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/NewsMonkey/static/media/loading.f4404720.gif"
+  },
+  {
+    "revision": "c061b5c9b73fc35a5539fe38dc9e1d1e",
+    "url": "/NewsMonkey/index.html"
+  }
+];
